71790b3caf1dfef0c2fd45d98b6ccbc7f8de74ec
kumanan <kumananboss@gmail.com>
2023-08-14 12:07:14 +0530

    auto_push

--- pythonscripts/01_script.py
+++ pythonscripts/01_script.py
@@ -10,11 +10,11 @@ def mes(status):
 
 
 cmd = ["sudo yum install httpd -y", "sudo systemctl enable httpd", "sudo systemctl restart httpd "]  
-for i in cmd:
-    if(os.system(i + '&>>/tmp/log.txt')) !=0:
-        print ("Faililed to Execute the Cmd: ")
-        exit (-1)
-print("HTTP configured Successfully")
+#for i in cmd:
+    #if(os.system(i + '&>>/tmp/log.txt')) !=0:
+        #print ("Faililed to Execute the Cmd: ")
+        #exit (-1)
+#print("HTTP configured Successfully")
 
 for i in cmd:
     status=(os.system(i + '&>>/tmp/log.txt'))
