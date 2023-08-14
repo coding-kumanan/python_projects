a4faa64413d0e50d58a00c7f2788832dbf3aeb87
kumanan <kumananboss@gmail.com>
2023-08-14 11:42:25 +0530

    auto_push

--- pythonscripts/01_script.py
+++ pythonscripts/01_script.py
@@ -3,7 +3,7 @@ import sys
 
 cmd= "sudo yum install htpd -y &>>/tmp/log.txt"
 if(os.system(cmd)) !=0:
-    print ("Faililed to Execute the Cmd" + cmd )
+    print ("Faililed to Execute the Cmd: " + cmd )
     exit (-1)
 else:
     print ("HTTPD installed Successfully")
