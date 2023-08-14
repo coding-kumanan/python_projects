f6980ebc05768dc8d438a6567c0869acf2511e33
kumanan <kumananboss@gmail.com>
2023-08-14 11:52:23 +0530

    auto_push

--- pythonscripts/01_script.py
+++ pythonscripts/01_script.py
@@ -1,10 +1,10 @@
 import os
 import sys
 
-cmd= "sudo yum install httpd -y"  
-#&>>/tmp/log.txt"
-if(os.system(cmd + '&>>/tmp/log.txt')) !=0:
-    print ("Faililed to Execute the Cmd: " + cmd )
-    exit (-1)
-else:
-    print ("HTTPD installed Successfully")
+cmd = ["sudo yum install httpd -y", "sudo systemctl enable httpd", "sudo systemctl restart httpd "]  
+for i in cmd:
+    if(os.system(i + '&>>/tmp/log.txt')) !=0:
+        print ("Faililed to Execute the Cmd: " + cmd )
+        exit (-1)
+    else:
+        print ("HTTPD installed Successfully\n Server Enabled and Started Successfully")
