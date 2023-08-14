de9596d853a99bfb9711a53b6251b3749d4daef7
kumanan <kumananboss@gmail.com>
2023-08-14 11:47:00 +0530

    auto_push

--- pythonscripts/01_script.py
+++ pythonscripts/01_script.py
@@ -1,8 +1,9 @@
 import os
 import sys
 
-cmd= "sudo yum install htpd -y &>>/tmp/log.txt"
-if(os.system(cmd)) !=0:
+cmd= "sudo yum install httpd -y"  
+#&>>/tmp/log.txt"
+if(os.system(cmd + '&>>/tmp/log.txt')) !=0:
     print ("Faililed to Execute the Cmd: " + cmd )
     exit (-1)
 else:
