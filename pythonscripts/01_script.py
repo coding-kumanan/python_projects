f859b6b898cd6be7bb88dbce4e3685b5e75169c3
kumanan <kumananboss@gmail.com>
2023-08-14 11:56:52 +0530

    auto_push

--- pythonscripts/01_script.py
+++ pythonscripts/01_script.py
@@ -4,6 +4,6 @@ import sys
 cmd = ["sudo yum install htpd -y", "sudo systemctl enable httpd", "sudo systemctl restart httpd "]  
 for i in cmd:
     if(os.system(i + '&>>/tmp/log.txt')) !=0:
-        print ("Faililed to Execute the Cmd: " + cmd )
+        print ("Faililed to Execute the Cmd: ")
         exit (-1)
 print("HTTP configured Successfully")
