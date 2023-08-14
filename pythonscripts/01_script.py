d9122b63a39c8f35b71bd25b22d643c425d89044
kumanan <kumananboss@gmail.com>
2023-08-14 12:19:47 +0530

    auto_push

--- pythonscripts/01_script.py
+++ pythonscripts/01_script.py
@@ -2,20 +2,12 @@ import os
 import sys
 def mes(status , i):
     if (status !=0):
-        print(i+ "failed")
+        print(i +  ":failed")
         exit(-1)
     else:
-         print(i+ "success")
+         print(i + ":success")
           
-    
-
-
-cmd = ["sudo yum install htttd -y", "sudo systemctl enable httpd", "sudo systemctl restart httpd "]  
-#for i in cmd:
-    #if(os.system(i + '&>>/tmp/log.txt')) !=0:
-        #print ("Faililed to Execute the Cmd: ")
-        #exit (-1)
-#print("HTTP configured Successfully")
+cmd = ["sudo yum install htttd -y", "sudo systemctl enable httpd", "sudo systemctl restart httpd "] 
 
 for i in cmd:
     status=(os.system(i + '&>>/tmp/log.txt'))
