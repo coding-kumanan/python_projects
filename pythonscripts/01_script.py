b3ac5cb2110e5c168d233970ce63a5b3b8e10af3
kumanan <kumananboss@gmail.com>
2023-08-14 11:55:12 +0530

    auto_push

--- pythonscripts/01_script.py
+++ pythonscripts/01_script.py
@@ -6,5 +6,4 @@ for i in cmd:
     if(os.system(i + '&>>/tmp/log.txt')) !=0:
         print ("Faililed to Execute the Cmd: " + cmd )
         exit (-1)
-    else:
-        print ("HTTPD installed Successfully\n Server Enabled and Started Successfully")
+print("HTTP configured Successfully")
