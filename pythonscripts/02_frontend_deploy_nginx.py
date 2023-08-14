a608499715a161ea11ee510d94a960b75626efcb
kumanan <kumananboss@gmail.com>
2023-08-14 13:07:51 +0530

    auto_push

--- pythonscripts/02_frontend_deploy_nginx.py
+++ pythonscripts/02_frontend_deploy_nginx.py
@@ -19,7 +19,7 @@ i='curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/front
 status=(os.system(i))
 mes(status, i)  
 print("---------------\nDeploy in Nginx Default Location")
-cmd = ["cd /usr/share/nginx/html", " rm -rf *", "unzip -o /tmp/frontend.zip",
+cmd = ["cd /usr/share/nginx/html", "rm -rf *", "unzip -o /tmp/frontend.zip",
        "mv frontend-main/* .","mv static/* .","rm -rf frontend-main README.md", 
        "mv localhost.conf /etc/nginx/default.d/roboshop.conf"]    
 for i in cmd:
