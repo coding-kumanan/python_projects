067cd0d2f02a969e87be791071e7a2557006559f
kumanan <kumananboss@gmail.com>
2023-08-14 11:28:41 +0530

    auto_push

--- pythonscripts/01_script.py
+++ pythonscripts/01_script.py
@@ -4,7 +4,7 @@ import sys
 cmd= "sudo yum install httpd -y"
 cmd1 = "/tmp/logfile.txt"
 os.system(cmd)
-id=os.system("$?") 
+id=os.system('$?') 
 if(id != 0):
     print ("Faililed to Execute the Cmd" + cmd )
     exit (-1)
